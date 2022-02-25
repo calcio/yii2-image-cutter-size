a6f13484462e5b878f3310f98afa9071427ed3c3
Cálcio <calcionit@gmail.com>
2022-02-25 11:40:33 -0300

    Update Yii widgets to call Bootstrap5

--- Cutter.php
+++ Cutter.php
@@ -5,8 +5,8 @@ namespace calcio\cutter;
 use Yii;
 use yii\helpers\Html;
 use yii\helpers\Json;
-use yii\bootstrap\Modal;
-use yii\bootstrap\ButtonGroup;
+use yii\bootstrap5\Modal;
+use yii\bootstrap5\ButtonGroup;
 
 /**
  * Class Cutter
@@ -79,11 +79,11 @@ class Cutter extends \yii\widgets\InputWidget
         echo Html::endTag('div');
 
         echo Html::checkbox($this->attribute . '-remove', false, [
-            'label' => Yii::t('mitrm/cutter/cutter', 'REMOVE')
+            'label' => Yii::t('calcio/cutter/cutter', 'REMOVE')
         ]);
 
         Modal::begin([
-            'header' => Html::tag('h4', Yii::t('mitrm/cutter/cutter', 'CUTTER')),
+            'header' => Html::tag('h4', Yii::t('calcio/cutter/cutter', 'CUTTER')),
             'closeButton' => [],
             'footer' => $this->getModalFooter($inputField),
             'size' => Modal::SIZE_LARGE,
@@ -97,32 +97,32 @@ class Cutter extends \yii\widgets\InputWidget
 
         echo Html::beginTag('div', ['class' => 'row']);
         echo Html::beginTag('div', ['class' => 'col-md-2']);
-        echo Html::label(Yii::t('mitrm/cutter/cutter', 'ASPECT_RATIO'), $inputField . '-aspectRatio');
+        echo Html::label(Yii::t('calcio/cutter/cutter', 'ASPECT_RATIO'), $inputField . '-aspectRatio');
         echo Html::textInput($this->attribute . '-aspectRatio', isset($this->cropperOptions['aspectRatio']) ? $this->cropperOptions['aspectRatio'] : 0, ['id' => $inputField . '-aspectRatio', 'class' => 'form-control']);
         echo Html::endTag('div');
 
         echo Html::beginTag('div', ['class' => 'col-md-2']);
-        echo Html::label(Yii::t('mitrm/cutter/cutter', 'ANGLE'), $inputField . '-dataRotate');
+        echo Html::label(Yii::t('calcio/cutter/cutter', 'ANGLE'), $inputField . '-dataRotate');
         echo Html::textInput($this->attribute . '-cropping[dataRotate]', '', ['id' => $inputField . '-dataRotate', 'class' => 'form-control']);
         echo Html::endTag('div');
 
         echo Html::beginTag('div', ['class' => 'col-md-2']);
-        echo Html::label(Yii::t('mitrm/cutter/cutter', 'POSITION') . ' (X)', $inputField . '-dataX');
+        echo Html::label(Yii::t('calcio/cutter/cutter', 'POSITION') . ' (X)', $inputField . '-dataX');
         echo Html::textInput($this->attribute . '-cropping[dataX]', '', ['id' => $inputField . '-dataX', 'class' => 'form-control']);
         echo Html::endTag('div');
 
         echo Html::beginTag('div', ['class' => 'col-md-2']);
-        echo Html::label(Yii::t('mitrm/cutter/cutter', 'POSITION') . ' (Y)', $inputField . '-dataY');
+        echo Html::label(Yii::t('calcio/cutter/cutter', 'POSITION') . ' (Y)', $inputField . '-dataY');
         echo Html::textInput($this->attribute . '-cropping[dataY]', '', ['id' => $inputField . '-dataY', 'class' => 'form-control']);
         echo Html::endTag('div');
 
         echo Html::beginTag('div', ['class' => 'col-md-2']);
-        echo Html::label(Yii::t('mitrm/cutter/cutter', 'WIDTH'), $inputField . '-dataWidth');
+        echo Html::label(Yii::t('calcio/cutter/cutter', 'WIDTH'), $inputField . '-dataWidth');
         echo Html::textInput($this->attribute . '-cropping[dataWidth]', '', ['id' => $inputField . '-dataWidth', 'class' => 'form-control']);
         echo Html::endTag('div');
 
         echo Html::beginTag('div', ['class' => 'col-md-2']);
-        echo Html::label(Yii::t('mitrm/cutter/cutter', 'HEIGHT'), $inputField . '-dataHeight');
+        echo Html::label(Yii::t('calcio/cutter/cutter', 'HEIGHT'), $inputField . '-dataHeight');
         echo Html::textInput($this->attribute . '-cropping[dataHeight]', '', ['id' => $inputField . '-dataHeight', 'class' => 'form-control']);
         echo Html::endTag('div');
         echo Html::endTag('div');
@@ -148,12 +148,12 @@ class Cutter extends \yii\widgets\InputWidget
 
     public function registerTranslations()
     {
-        Yii::$app->i18n->translations['mitrm/cutter/*'] = [
+        Yii::$app->i18n->translations['calcio/cutter/*'] = [
             'class' => 'yii\i18n\PhpMessageSource',
             'sourceLanguage' => 'en-US',
-            'basePath' => '@vendor/mitrm/yii2-image-cutter/messages',
+            'basePath' => '@vendor/calcio/yii2-image-cutter/messages',
             'fileMap' => [
-                'mitrm/cutter/cutter' => 'cutter.php',
+                'calcio/cutter/cutter' => 'cutter.php',
             ],
         ];
     }
@@ -163,153 +163,153 @@ class Cutter extends \yii\widgets\InputWidget
         return Html::beginTag('div', [
             'class' => 'btn-toolbar pull-left'
         ]) .
-        ButtonGroup::widget([
-            'encodeLabels' => false,
-            'buttons' => [
-                [
-                    'label' => '<i class="glyphicon glyphicon-move"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'setDragMode',
-                        'data-option' => 'move',
-                        'class' => 'btn btn-primary',
-                        'title' => Yii::t('mitrm/cutter/cutter', 'DRAG_MODE_MOVE'),
-                    ]
-                ],
-                [
-                    'label' => '<i class="glyphicon glyphicon-scissors"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'setDragMode',
-                        'data-option' => 'crop',
-                        'class' => 'btn btn-primary',
-                        'data-title' => Yii::t('mitrm/cutter/cutter', 'DRAG_MODE_CROP'),
-                    ]
-                ],
-            ],
-            'options' => [
-                'class' => 'pull-left'
-            ]
-        ]) .
-        ButtonGroup::widget([
-            'encodeLabels' => false,
-            'buttons' => [
-                [
-                    'label' => '<i class="glyphicon glyphicon-ok"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'crop',
-                        'class' => 'btn btn-primary',
-                        'data-title' => Yii::t('mitrm/cutter/cutter', 'CROP'),
-                    ]
-                ],
-                [
-                    'label' => '<i class="glyphicon glyphicon-refresh"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'reset',
-                        'class' => 'btn btn-primary',
-                        'title' => Yii::t('mitrm/cutter/cutter', 'REFRESH'),
-                    ]
-                ],
-                [
-                    'label' => '<i class="glyphicon glyphicon-remove"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'clear',
-                        'class' => 'btn btn-primary',
-                        'title' => Yii::t('mitrm/cutter/cutter', 'REMOVE'),
-                    ]
-                ],
-            ],
-            'options' => [
-                'class' => 'pull-left'
-            ]
-        ]) .
-        ButtonGroup::widget([
-            'encodeLabels' => false,
-            'buttons' => [
-                [
-                    'label' => '<i class="glyphicon glyphicon-zoom-in"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'zoom',
-                        'data-option' => '0.1',
-                        'class' => 'btn btn-primary',
-                        'title' => Yii::t('mitrm/cutter/cutter', 'ZOOM_IN'),
+            ButtonGroup::widget([
+                'encodeLabels' => false,
+                'buttons' => [
+                    [
+                        'label' => '<i class="glyphicon glyphicon-move"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'setDragMode',
+                            'data-option' => 'move',
+                            'class' => 'btn btn-primary',
+                            'title' => Yii::t('calcio/cutter/cutter', 'DRAG_MODE_MOVE'),
+                        ]
                     ],
-                    'visible' => $this->cropperOptions['zoomable']
-                ],
-                [
-                    'label' => '<i class="glyphicon glyphicon-zoom-out"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'zoom',
-                        'data-option' => '-0.1',
-                        'class' => 'btn btn-primary',
-                        'title' => Yii::t('mitrm/cutter/cutter', 'ZOOM_OUT'),
+                    [
+                        'label' => '<i class="glyphicon glyphicon-scissors"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'setDragMode',
+                            'data-option' => 'crop',
+                            'class' => 'btn btn-primary',
+                            'data-title' => Yii::t('calcio/cutter/cutter', 'DRAG_MODE_CROP'),
+                        ]
                     ],
-                    'visible' => $this->cropperOptions['zoomable']
                 ],
-                [
-                    'label' => '<i class="glyphicon glyphicon-share-alt  icon-flipped"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'rotate',
-                        'data-option' => '45',
-                        'class' => 'btn btn-primary',
-                        'title' => Yii::t('mitrm/cutter/cutter', 'ROTATE_LEFT'),
+                'options' => [
+                    'class' => 'pull-left'
+                ]
+            ]) .
+            ButtonGroup::widget([
+                'encodeLabels' => false,
+                'buttons' => [
+                    [
+                        'label' => '<i class="glyphicon glyphicon-ok"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'crop',
+                            'class' => 'btn btn-primary',
+                            'data-title' => Yii::t('calcio/cutter/cutter', 'CROP'),
+                        ]
                     ],
-                    'visible' => $this->cropperOptions['rotatable']
-                ],
-                [
-                    'label' => '<i class="glyphicon glyphicon-share-alt"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'rotate',
-                        'data-option' => '-45',
-                        'class' => 'btn btn-primary',
-                        'title' => Yii::t('mitrm/cutter/cutter', 'ROTATE_RIGHT'),
+                    [
+                        'label' => '<i class="glyphicon glyphicon-refresh"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'reset',
+                            'class' => 'btn btn-primary',
+                            'title' => Yii::t('calcio/cutter/cutter', 'REFRESH'),
+                        ]
                     ],
-                    'visible' => $this->cropperOptions['rotatable']
+                    [
+                        'label' => '<i class="glyphicon glyphicon-remove"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'clear',
+                            'class' => 'btn btn-primary',
+                            'title' => Yii::t('calcio/cutter/cutter', 'REMOVE'),
+                        ]
+                    ],
                 ],
-            ],
-            'options' => [
-                'class' => 'pull-left'
-            ]
-        ]) .
-        ButtonGroup::widget([
-            'encodeLabels' => false,
-            'buttons' => [
-                [
-                    'label' => '<i class="glyphicon glyphicon-glyphicon glyphicon-resize-full"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'setAspectRatio',
-                        'data-target' => '#' . $inputField . '-aspectRatio',
-                        'class' => 'btn btn-primary',
-                        'title' => Yii::t('mitrm/cutter/cutter', 'SET_ASPECT_RATIO'),
-                    ]
+                'options' => [
+                    'class' => 'pull-left'
+                ]
+            ]) .
+            ButtonGroup::widget([
+                'encodeLabels' => false,
+                'buttons' => [
+                    [
+                        'label' => '<i class="glyphicon glyphicon-zoom-in"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'zoom',
+                            'data-option' => '0.1',
+                            'class' => 'btn btn-primary',
+                            'title' => Yii::t('calcio/cutter/cutter', 'ZOOM_IN'),
+                        ],
+                        'visible' => $this->cropperOptions['zoomable']
+                    ],
+                    [
+                        'label' => '<i class="glyphicon glyphicon-zoom-out"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'zoom',
+                            'data-option' => '-0.1',
+                            'class' => 'btn btn-primary',
+                            'title' => Yii::t('calcio/cutter/cutter', 'ZOOM_OUT'),
+                        ],
+                        'visible' => $this->cropperOptions['zoomable']
+                    ],
+                    [
+                        'label' => '<i class="glyphicon glyphicon-share-alt  icon-flipped"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'rotate',
+                            'data-option' => '45',
+                            'class' => 'btn btn-primary',
+                            'title' => Yii::t('calcio/cutter/cutter', 'ROTATE_LEFT'),
+                        ],
+                        'visible' => $this->cropperOptions['rotatable']
+                    ],
+                    [
+                        'label' => '<i class="glyphicon glyphicon-share-alt"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'rotate',
+                            'data-option' => '-45',
+                            'class' => 'btn btn-primary',
+                            'title' => Yii::t('calcio/cutter/cutter', 'ROTATE_RIGHT'),
+                        ],
+                        'visible' => $this->cropperOptions['rotatable']
+                    ],
                 ],
-                [
-                    'label' => '<i class="glyphicon glyphicon-upload"></i>',
-                    'options' => [
-                        'type' => 'button',
-                        'data-method' => 'setData',
-                        'class' => 'btn btn-primary',
-                        'title' => Yii::t('mitrm/cutter/cutter', 'SET_DATA'),
-                    ]
+                'options' => [
+                    'class' => 'pull-left'
+                ]
+            ]) .
+            ButtonGroup::widget([
+                'encodeLabels' => false,
+                'buttons' => [
+                    [
+                        'label' => '<i class="glyphicon glyphicon-glyphicon glyphicon-resize-full"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'setAspectRatio',
+                            'data-target' => '#' . $inputField . '-aspectRatio',
+                            'class' => 'btn btn-primary',
+                            'title' => Yii::t('calcio/cutter/cutter', 'SET_ASPECT_RATIO'),
+                        ]
+                    ],
+                    [
+                        'label' => '<i class="glyphicon glyphicon-upload"></i>',
+                        'options' => [
+                            'type' => 'button',
+                            'data-method' => 'setData',
+                            'class' => 'btn btn-primary',
+                            'title' => Yii::t('calcio/cutter/cutter', 'SET_DATA'),
+                        ]
+                    ],
                 ],
-            ],
-            'options' => [
-                'class' => 'pull-left'
-            ]
-        ]) .
-        Html::endTag('div') .
-        Html::button(Yii::t('mitrm/cutter/cutter', 'CANCEL'), [
-            'id' => $this->imageOptions['id'] . '_button_cancel', 'class' => 'btn btn-danger'
-        ]) . Html::button(Yii::t('mitrm/cutter/cutter', 'ACCEPT'), [
-            'id' => $this->imageOptions['id'] . '_button_accept', 'class' => 'btn btn-success'
-        ]);
+                'options' => [
+                    'class' => 'pull-left'
+                ]
+            ]) .
+            Html::endTag('div') .
+            Html::button(Yii::t('calcio/cutter/cutter', 'CANCEL'), [
+                'id' => $this->imageOptions['id'] . '_button_cancel', 'class' => 'btn btn-danger'
+            ]) . Html::button(Yii::t('calcio/cutter/cutter', 'ACCEPT'), [
+                'id' => $this->imageOptions['id'] . '_button_accept', 'class' => 'btn btn-success'
+            ]);
     }
 }
